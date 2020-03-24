7cddbd003754aa1bb315452d61097ecc96203f94
sarthakdandotiya <sarthak.dandotiya23@gmail.com>
2020-03-24 05:39:39 +0530

    Add GET Requests

--- src/main/java/me/sarthak/springbootstarter/topic/TopicController.java
+++ src/main/java/me/sarthak/springbootstarter/topic/TopicController.java
@@ -1,10 +1,10 @@
 package me.sarthak.springbootstarter.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -18,4 +18,8 @@ public class TopicController {
 		return topicService.getAllTopics();
 	}
 	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
 }
